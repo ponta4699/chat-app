bbbfd969b605346ccf795d8b9e6321332e9a2819
ponta4699 <ponta4699@gmail.com>
2022-10-28 17:03:17 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,9 +18,7 @@ class MessagesController < ApplicationController
    end
  
    private
- 
-   def message_params
-     params.require(:message).permit(:content).merge(user_id: current_user.id)
-   end
- end
- 
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
